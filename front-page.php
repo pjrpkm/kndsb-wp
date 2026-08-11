9e191e1bb5cf2fc0b90da44ba7b5e251d42174bb
pjrpkm <pjrpkm@gmail.com>
2026-08-11 14:16:53 +0200

    Remove WP Pusher deployment test

--- front-page.php
+++ front-page.php
@@ -10,7 +10,6 @@ defined( 'ABSPATH' ) || exit;
 get_header();
 ?>
 <main id="main-content" class="main-wrapper kndsb-home" tabindex="-1">
-	<div style="position:relative;z-index:9999;padding:12px;text-align:center;font-weight:700;">TEST</div>
 	<?php while ( have_posts() ) : the_post(); ?>
 		<?php if ( has_block( 'kndsb/layout-section', get_the_content() ) ) : ?>
 			<?php the_content(); ?>

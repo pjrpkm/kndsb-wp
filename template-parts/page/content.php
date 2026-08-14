05f95310f655c327fcd3c9c9f31cfe48e56a23aa
pjrpkm <pjrpkm@gmail.com>
2026-08-14 23:15:34 +0200

    Generate page intro from WordPress page hierarchy

--- template-parts/page/content.php
+++ template-parts/page/content.php
@@ -2,17 +2,25 @@
 /**
  * Default Gutenberg page content.
  *
+ * The page shell owns the eyebrow and title. Gutenberg owns the page content.
+ *
  * @package KNDSB
  */
 
 defined( 'ABSPATH' ) || exit;
+
+$parent_id = wp_get_post_parent_id( get_the_ID() );
 ?>
 <article id="post-<?php the_ID(); ?>" <?php post_class( 'kndsb-page__article' ); ?>>
 	<?php if ( ! is_front_page() ) : ?>
-		<header class="kndsb-page__header">
-			<h1 class="kndsb-page__title"><?php the_title(); ?></h1>
+		<header class="kndsb-page-intro wp-block-kndsb-page-intro">
+			<?php if ( $parent_id ) : ?>
+				<p class="kndsb-page-intro__eyebrow"><?php echo esc_html( get_the_title( $parent_id ) ); ?></p>
+			<?php endif; ?>
+			<h1 class="kndsb-page-intro__title"><?php the_title(); ?></h1>
 		</header>
 	<?php endif; ?>
+
 	<div class="kndsb-page__content">
 		<?php the_content(); ?>
 	</div>

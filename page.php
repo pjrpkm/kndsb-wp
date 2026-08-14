d6388d6f320c0804ce930f6fcf56e7a2fa8d8f6d
pjrpkm <pjrpkm@gmail.com>
2026-08-14 23:19:28 +0200

    Route all standard pages through generic Gutenberg shell

--- page.php
+++ page.php
@@ -1,6 +1,8 @@
 <?php
 /**
- * Clean BEM template for every WordPress page.
+ * Generic Gutenberg template for WordPress pages.
+ *
+ * PHP owns the fixed page shell; Gutenberg owns page content.
  *
  * @package KNDSB
  */
@@ -11,13 +13,7 @@ get_header();
 ?>
 <main id="main-content" class="main-wrapper kndsb-page" tabindex="-1">
 	<?php while ( have_posts() ) : the_post(); ?>
-		<?php if ( has_block( 'kndsb/layout-section', get_the_content() ) ) : ?>
-			<?php the_content(); ?>
-		<?php else : ?>
-			<section class="section_page-content"><div class="padding-global"><div class="container-large"><div class="padding-section-medium">
-				<?php get_template_part( 'template-parts/page/content' ); ?>
-			</div></div></div></section>
-		<?php endif; ?>
+		<?php get_template_part( 'template-parts/page/content' ); ?>
 	<?php endwhile; ?>
 </main>
 <?php

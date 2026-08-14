f1cded00f7e3f6129381767965a1a8ffb8b912cf
pjrpkm <pjrpkm@gmail.com>
2026-08-14 23:27:12 +0200

    Hide generic page intro on individual sport pages

--- template-parts/page/content.php
+++ template-parts/page/content.php
@@ -10,9 +10,15 @@
 defined( 'ABSPATH' ) || exit;
 
 $parent_id = (int) get_post_field( 'post_parent', get_the_ID() );
+$content   = (string) get_the_content();
+
+/* Individual sport pages own their visual hero/title inside Gutenberg. */
+$is_individual_sport = false !== strpos( $content, 'kndsb-sport-page' )
+	&& ! is_page( array( 'sport', 'sporten', 'sporttakken' ) );
+$show_page_intro = ! is_front_page() && ! $is_individual_sport;
 ?>
 <article id="post-<?php the_ID(); ?>" <?php post_class( 'kndsb-page__article' ); ?>>
-	<?php if ( ! is_front_page() ) : ?>
+	<?php if ( $show_page_intro ) : ?>
 		<section class="section_page-intro">
 			<div class="padding-global">
 				<div class="container-large">

c94b134a02b0159679325d806a74dd5760766e78
pjrpkm <pjrpkm@gmail.com>
2026-08-14 21:17:33 +0200

    Add related article grid to single posts

--- single.php
+++ single.php
@@ -36,6 +36,48 @@ get_header();
 					</div>
 				</div>
 			</div>
+
+			<?php
+			$related_args = array(
+				'post_type'           => get_post_type(),
+				'post_status'         => 'publish',
+				'posts_per_page'      => 3,
+				'post__not_in'        => array( get_the_ID() ),
+				'ignore_sticky_posts' => true,
+			);
+
+			$category_ids = wp_get_post_categories( get_the_ID() );
+			if ( ! empty( $category_ids ) ) {
+				$related_args['category__in'] = $category_ids;
+			}
+
+			$related_articles = new WP_Query( $related_args );
+			?>
+			<?php if ( $related_articles->have_posts() ) : ?>
+				<section class="kndsb-related" aria-labelledby="kndsb-related-title">
+					<div class="kndsb-related__inner">
+						<h2 id="kndsb-related-title" class="kndsb-related__title">Gerelateerde artikelen</h2>
+						<div class="kndsb-related__grid">
+							<?php while ( $related_articles->have_posts() ) : $related_articles->the_post(); ?>
+								<article class="kndsb-related__card">
+									<a class="kndsb-related__link" href="<?php the_permalink(); ?>">
+										<?php if ( has_post_thumbnail() ) : ?>
+											<div class="kndsb-related__image-wrap">
+												<?php the_post_thumbnail( 'medium_large', array( 'class' => 'kndsb-related__image', 'loading' => 'lazy' ) ); ?>
+											</div>
+										<?php endif; ?>
+										<div class="kndsb-related__content">
+											<time class="kndsb-related__date" datetime="<?php echo esc_attr( get_the_date( DATE_W3C ) ); ?>"><?php echo esc_html( get_the_date() ); ?></time>
+											<h3 class="kndsb-related__card-title"><?php the_title(); ?></h3>
+										</div>
+									</a>
+								</article>
+							<?php endwhile; ?>
+						</div>
+					</div>
+				</section>
+			<?php endif; ?>
+			<?php wp_reset_postdata(); ?>
 		</article>
 	<?php endwhile; ?>
 </main>
